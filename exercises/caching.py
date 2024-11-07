0529e5a5c7c906c5bfa4c71bf449074e42e93cbe
Rich Benjamin <richmwendwa03@gmail.com>
2024-11-08 00:03:50 +0300

    Using dictionaries to build a caching system

--- exercises/caching.py
+++ exercises/caching.py
@@ -2,11 +2,10 @@ cache = {} # initializes an empty dictionary named cache, which will be used to 
 
 def cached_factorial(n): #  defines the cached_factorial function, which calculates the factorial of a given number n 
 
-
-    # Check if the result is already in the cache
-    if n in cache:
-        print(f"Fetching from cache for n={n}")
+    if n in cache: # checks whether n is already in the cache
+        print(f"Fetching from cache for n={n}") # This line logs that the function is retrieving a cached result, making it clear in the output when the cache is being used.
         return cache[n]
+    
     result = 1
     for i in range(1, n + 1):
         result *= i
