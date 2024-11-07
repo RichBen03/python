1a1cf57db9449c7d5b251f4c2a3d08cd90127e43
Rich Benjamin <richmwendwa03@gmail.com>
2024-11-07 23:37:11 +0300

    Using dictionaries to build a caching system

--- exercises/decorator.py
+++ exercises/decorator.py
@@ -2,8 +2,8 @@
 
 import time #importing the time library
 
-def timer_decorator(func): # creating a fuunction that takes another function as input
-    def wrapper(*args, **kwargs): #
+def timer_decorator(func): # creating a fuunction that takes another function as input.The function that will run is finc
+    def wrapper(*args, **kwargs): #  defining another function called wrapper that will run instead of the original function (func). The *args and **kwargs allow wrapper to accept any number of arguments and keyword arguments, just like the original function.
         start_time = time.time()  # getting the current time in seconds and store it in start_time. This is the moment just before func is called.
         result = func(*args, **kwargs)  # Execute the function and store its result
         end_time = time.time()  # Record the end time after the function completes
