031beddcac77c9d809aa36af4851e513d5ca0956
Rich Benjamin <richmwendwa03@gmail.com>
2024-11-08 22:29:06 +0300

    A MINI authentication system:

--- exercises/authentication.py
+++ exercises/authentication.py
@@ -7,4 +7,17 @@ class AccountLocked(Exception):
     pass
 
 class InsufficientPrivileges(Exception):
-    """Raised 
+    """Raised when a user does not have sufficient privileges."""
+    pass
+
+class AuthSystem:
+    def __init__(self):
+        # Predefined username and password
+        self.username = "admin"
+        self.password = "password123"
+        # Privileges dictionary for user roles
+        self.privileges = {"admin": "all_access", "user": "limited_access"}
+        # Maximum number of allowed attempts
+        self.max_attempts = 3
+        self.attempts = 0
+        self.locked = False
